b01c3bda46c87aedf9862b1d3bff085ad0789a43
Christina Senina <christina.senina@mail.ru>
2024-03-19 02:34:23 +0300

    завершает блок about

---
+++ source/js/video.js
@@ -0,0 +1,50 @@
+export function findVideos() {
+  const videos = document.querySelectorAll('.about__video');
+
+  for (let i = 0; i < videos.length; i++) {
+    setupVideo(videos[i]);
+  }
+}
+
+function setupVideo(video) {
+  const link = video.querySelector('.about__link');
+  const media = video.querySelector('.about__cover');
+  const button = video.querySelector('.about__button');
+  const id = parseMediaURL(media);
+
+  video.addEventListener('click', () => {
+    const iframe = createIframe(id);
+
+    link.remove();
+    button.remove();
+    video.appendChild(iframe);
+  });
+
+  link.removeAttribute('href');
+  video.classList.add('about__video--enabled');
+}
+
+function parseMediaURL(media) {
+  const regexp = /img\/about\/about-mobile\.jpg/i;
+  const url = media.src;
+  const match = url.match(regexp);
+
+  return match;
+}
+
+function createIframe(id) {
+  const iframe = document.createElement('iframe');
+
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('src', generateURL(id));
+  iframe.classList.add('about__cover');
+
+  return iframe;
+}
+
+function generateURL(id) {
+  const query = 'watch?v=9TZXsZItgdw&autoplay=1';
+
+  return `https://www.youtube.com/embed/${id}${query}`;
+}
+
