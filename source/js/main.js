04ddb49c6d651f8f16d9cab017eac68c9f75b193
Christina Senina <christina.senina@mail.ru>
2024-03-29 19:22:18 +0300

    стилизует блок faq tablet/desktop

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,13 @@
 import { findVideos } from './video.js';
 import { openTabs } from './tab-price.js';
-
 import { playAccordion } from './accordion.js';
 import { openTabsSecond } from './tab-faq.js';
+import { slider } from './slider.js';
 
 findVideos ();
 openTabs ();
-
+slider ();
 playAccordion ();
 openTabsSecond ();
+
 
--- source/js/slider.js
+++ source/js/slider.js
@@ -1,52 +1,53 @@
 import { Swiper } from 'swiper';
 import { Navigation } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
 
 
+export function slider() {
+  new Swiper('.juri__slider', {
+    modules: [Navigation, Autoplay],
 
-new Swiper('.swiper', {
-  modules: [Navigation],
+    direction: 'horizontal',
+    loop: true,
 
-  direction: 'horizontal',
-  loop: true,
+    slidesPerView: 'auto',
 
-  slidesPerView: 'auto',
-
-  autoplay: {
-    delay: 3000,
-    stopOnLastSlide: false,
-    disableOnInteraction: true,
-  },
-
-  // speed: 1000,
-  // freeMode: true,
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  breakpoints: {
-    320: {
-      width: 227,
-      slidesPerView: 1,
-      initialSlide: 2,
-      spaceBetween: 40,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: true,
     },
 
-    768: {
-      width: 560,
-      slidesPerView: 2,
-      initialSlide: 0,
-      spaceBetween: 40,
+    speed: 1000,
+    freeMode: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
 
-    1366: {
-      width: 1160,
-      slidesPerView: 4,
-      initialSlide: 0,
-      spaceBetween: 40,
+    breakpoints: {
+      320: {
+        width: 227,
+        slidesPerView: 1,
+        initialSlide: 2,
+        spaceBetween: 40,
+      },
+
+      768: {
+        width: 560,
+        slidesPerView: 2,
+        initialSlide: 0,
+        spaceBetween: 40,
+      },
+
+      1366: {
+        width: 1160,
+        slidesPerView: 4,
+        initialSlide: 0,
+        spaceBetween: 40,
+      }
     }
-  }
-});
+  });
+}
 
 
