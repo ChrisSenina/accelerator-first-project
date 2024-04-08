de08be4ebe243b494570341e54c7cf86dea93aa4
Christina Senina <christina.senina@mail.ru>
2024-04-08 14:16:03 +0300

    Вносит правки

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -20,15 +20,13 @@ export function playAccordion() {
   });
 
   accordionContent.forEach((item) => {
-    // eslint-disable-next-line prefer-const
-    let header = item.querySelector('h3');
+    const header = item.querySelector('h3');
     header.addEventListener('click', () => {
       item.classList.toggle('open');
 
-      // eslint-disable-next-line prefer-const
-      let description = item.querySelector('p');
-      // eslint-disable-next-line prefer-const
-      let input = item.querySelector('.accordion__input');
+      const description = item.querySelector('p');
+
+      const input = item.querySelector('.accordion__input');
       if (item.classList.contains('open')) {
         input.classList.add('open');
         description.style.height = `${description.scrollHeight}px`;
