233e8f09822acf3cbe23ebf83780edd54bfdc1f9
Christina Senina <christina.senina@mail.ru>
2024-04-28 17:37:06 +0300

    footer tablet+desktop

--- source/js/form.js
+++ source/js/form.js
@@ -4,35 +4,35 @@ const emailInput = form.querySelector('.form__input-email');
 
 export function isValidate() {
   form.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-  });
 
+    phoneInput.addEventListener('input', () => {
+      phoneInput.setCustomValidity('');
+      phoneInput.checkValidity();
+    });
 
-  phoneInput.addEventListener('input', () => {
-    phoneInput.setCustomValidity('');
-    phoneInput.checkValidity();
-  });
+    phoneInput.addEventListener('invalid', () => {
+      if (phoneInput.matches('^((\+7|)+([0-9]){10})$')) {
+        SubmitEvent();
+      } else {
+        evt.preventDefault();
+        phoneInput.classList.add('form__input-error');
+        phoneInput.setCustomValidity('Введите телефон в формате: +79996663322');
+      }
+    });
 
-  phoneInput.addEventListener('invalid', () => {
-    if(phoneInput.value === '') {
-      phoneInput.classList.add('form__input-error');
-      phoneInput.setCustomValidity('Введите телефон в формате: +79996663322');
-    } else {
-      SubmitEvent();
-    }
-  });
+    emailInput.addEventListener('input', () => {
+      emailInput.setCustomValidity('');
+      emailInput.checkValidity();
+    });
 
-  emailInput.addEventListener('input', () => {
-    emailInput.setCustomValidity('');
-    emailInput.checkValidity();
-  });
-
-  emailInput.addEventListener('invalid', () => {
-    if(emailInput.value === '') {
-      emailInput.classList.add('form__input-error');
-      emailInput.setCustomValidity('Введите email в формате: example@example.ru');
-    } else {
-      SubmitEvent();
-    }
+    emailInput.addEventListener('invalid', () => {
+      if (emailInput.matches('[a-zA-Zа-яА-Я0-9_\-\.]{2,}@[a-zA-Zа-яА-Я]{2,}\.[a-zA-Zа-яА-Я]{2,}')) {
+        SubmitEvent();
+      } else {
+        evt.preventDefault();
+        emailInput.classList.add('form__input-error');
+        emailInput.setCustomValidity('Введите email в формате: example@example.ru');
+      }
+    });
   });
 }
