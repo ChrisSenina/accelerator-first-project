3b5aaef1199f11c330fe3d1fc14db8c89051426b
Christina Senina <christina.senina@mail.ru>
2024-05-23 18:15:18 +0300

    header nav

---
+++ source/js/nav.js
@@ -0,0 +1,24 @@
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav__link');
+const menuButton = document.querySelector('.nav__toggle');
+
+export function navOpen() {
+
+  nav.classList.add('nav--closed');
+
+  menuButton.addEventListener('click', () => {
+    nav.classList.toggle('nav--closed');
+    nav.classList.toggle('nav--opened');
+    // document.body.style.position = 'fixed';
+    // document.body.style.width = '100%';
+  });
+
+  navLinks.forEach((navLink) => {
+    navLink.addEventListener('click', () => {
+      nav.classList.remove('nav--opened');
+      nav.classList.add('nav--closed');
+      // document.body.style.position = '';
+      // document.body.style.width = '';
+    });
+  });
+}
