2746b0df28e2f21c67a972fb3310c2020ede1537
Christina Senina <christina.senina@mail.ru>
2024-05-29 15:06:11 +0300

    about tablet

--- pp.config.cjs
+++ pp.config.cjs
@@ -13,7 +13,7 @@ const desktopSections = [
 
 const tabletSections = [
   {section: 'hero', misMatchThreshold: 0.5},
-  // {section: 'about', misMatchThreshold: 0.5},
+  {section: 'about', misMatchThreshold: 0.5},
   // {section: 'programs', misMatchThreshold: 0.7},
   // {section: 'grant', misMatchThreshold: 0.5},
   // {section: 'news', misMatchThreshold: 0.7},
