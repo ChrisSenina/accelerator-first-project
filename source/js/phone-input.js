af93fe39cc7b8695c564c48082ec00dce23a989c
Christina Senina <christina.senina@mail.ru>
2024-06-07 17:57:12 +0300

    modal width change + input mask js changes

--- source/js/phone-input.js
+++ source/js/phone-input.js
@@ -3,7 +3,7 @@ export function inputMask() {
     const phoneInputs = document.querySelectorAll('input[data-tel-input]');
 
     const getInputNumbersValue = function (input) {
-      // Return stripped input value — just numbers
+      // Вырезаем все символы, кроме цифр
       return input.value.replace(/\D/g, '');
     };
 
@@ -14,10 +14,9 @@ export function inputMask() {
       if (pasted) {
         const pastedText = pasted.getData('Text');
         if (/\D/g.test(pastedText)) {
-          // Attempt to paste non-numeric symbol — remove all non-numeric symbols,
-          // formatting will be in onPhoneInput handler
+          // Если происходит попытка ввода нечисловых символов - удляем их
+          // сбрасыываем форматирование
           input.value = inputNumbersValue;
-
         }
       }
     };
@@ -36,9 +35,9 @@ export function inputMask() {
       }
 
       if (input.value.length !== selectionStart) {
-        // Editing in the middle of input, not last symbol
+        // форматирование в середение строки
         if (e.data && /\D/g.test(e.data)) {
-          // Attempt to input non-numeric symbol
+          // если вводят нечисловые симоволы - сброс
           input.value = inputNumbersValue;
         }
         return;
@@ -46,38 +45,39 @@ export function inputMask() {
 
       if (['7', '8', '9'].indexOf(inputNumbersValue[0]) > -1) {
         if (inputNumbersValue[0] === '9') {
-          inputNumbersValue = `7${ inputNumbersValue}`;
+          inputNumbersValue = `7${inputNumbersValue}`;
         }
         const firstSymbols = (inputNumbersValue[0] === '8') ? '8' : '+7';
-        formattedInputValue = input.value = `${firstSymbols } `;
+        formattedInputValue = input.value = `${firstSymbols} `;
         if (inputNumbersValue.length > 1) {
-          formattedInputValue += `(${ inputNumbersValue.substring(1, 4)}`;
+          formattedInputValue += `(${inputNumbersValue.substring(1, 4)}`;
         }
         if (inputNumbersValue.length >= 5) {
-          formattedInputValue += `) ${ inputNumbersValue.substring(4, 7)}`;
+          formattedInputValue += `) ${inputNumbersValue.substring(4, 7)}`;
         }
         if (inputNumbersValue.length >= 8) {
-          formattedInputValue += `-${ inputNumbersValue.substring(7, 9)}`;
+          formattedInputValue += `-${inputNumbersValue.substring(7, 9)}`;
         }
         if (inputNumbersValue.length >= 10) {
-          formattedInputValue += `-${ inputNumbersValue.substring(9, 11)}`;
+          formattedInputValue += `-${inputNumbersValue.substring(9, 11)}`;
         }
       } else {
-        formattedInputValue = `+${ inputNumbersValue.substring(0, 16)}`;
+        formattedInputValue = `+${inputNumbersValue.substring(0, 16)}`;
       }
       input.value = formattedInputValue;
     };
     const onPhoneKeyDown = function (e) {
-      // Clear input after remove last symbol
+      // Очистка инпута после ввода последнего символа
       const inputValue = e.target.value.replace(/\D/g, '');
       if (e.keyCode === 8 && inputValue.length === 1) {
         e.target.value = '';
       }
     };
-    for (const phoneInput of phoneInputs) {
-      phoneInput.addEventListener('keydown', onPhoneKeyDown);
-      phoneInput.addEventListener('input', onPhoneInput, false);
-      phoneInput.addEventListener('paste', onPhonePaste, false);
+    for (let i = 0; i < phoneInputs.length; i++) {
+      let input = phoneInputs[i];
+      input.addEventListener('keydown', onPhoneKeyDown);
+      input.addEventListener('input', onPhoneInput, false);
+      input.addEventListener('paste', onPhonePaste, false);
     }
   });
 }
